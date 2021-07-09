07f8b5e4ca59b510dd2efd3e4a6904295577fc29
demolitionok <demolitionok@gmail.com>
2021-07-09 16:14:27 +0300

    fix: fix incorrect King's GetRelativeMoves filling

--- Assets/Scripts/Figures/King.cs
+++ Assets/Scripts/Figures/King.cs
@@ -20,20 +20,15 @@ public class King : Figure
             var direction = new List<(int, int)>();
             result.Add(direction);
         }
-        for (int k = 1; k < size.Item1; k++)
-        {
-            result[0].Add((1,1));
-            result[1].Add((0,1));
-            result[2].Add((0,-1));
-            result[3].Add((-1,0));
-        }
-        for (int k = 1; k < size.Item2; k++)
-        {
-            result[4].Add((-1,-1));
-            result[5].Add((1,-1));
-            result[6].Add((-1,1));
-            result[7].Add((1, 0));
-        }
+        result[0].Add((1,1));
+        result[1].Add((0,1));
+        result[2].Add((0,-1));
+        result[3].Add((-1,0));
+        result[4].Add((-1,-1));
+        result[5].Add((1,-1));
+        result[6].Add((-1,1));
+        result[7].Add((1, 0));
+        
         return result;
             
     }
