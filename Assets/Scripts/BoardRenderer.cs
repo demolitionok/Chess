8ff3a234464dd2d7dee3a3996eada02d36961a46
demolitionok <demolitionok@gmail.com>
2021-07-19 13:54:22 +0300

    refactor: get rid of static by creating game class

---
+++ Assets/Scripts/BoardRenderer.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BoardRenderer
+{
+    private readonly Func<(int, int), Cell> GetCellByCoords;
+
+    public void ColorButton(Button button, Color color)
+    {
+        var colors = button.colors;
+        colors.normalColor = color;
+        colors.selectedColor = color;
+        button.colors = colors;
+    }
+
+    private void ColorCells(List<(int, int)> cellsCoordsToColor, Color color)
+    {
+        foreach (var coords in cellsCoordsToColor)
+        {
+            var curCell = GetCellByCoords(coords);
+            var button = curCell.gameObject.GetComponent<Button>();
+            ColorButton(button, color);
+        }
+    }
+
+    public void RenderAttacks(List<(int, int)> possibleAttacks) => ColorCells(possibleAttacks, Color.red);
+    public void RenderMoves(List<(int, int)> possibleMoves) => ColorCells(possibleMoves, Color.blue);
+
+    public void SetFigureImageByCoords((int, int) coords, Sprite sprite)
+    {
+        GetCellByCoords(coords).gameObject.GetComponent<Image>().sprite = sprite;
+    }
+
+    public void SetCellInfoText((int, int) coords)
+    {
+        var cell = GetCellByCoords(coords);
+        var textTranform = cell.gameObject.transform.GetChild(0);
+        var textStr = $" {cell.Figure.Side}";
+        textStr += $"\n {coords.Item2}, {coords.Item1}";
+        var figureName = cell.Figure == null ? "noFigure" : cell.Figure.Name;
+        textStr += $"\n {figureName}";
+        textTranform.gameObject.GetComponent<Text>().text = textStr;
+    }
+
+    public BoardRenderer(Func<(int, int), Cell> getCellByCoords)
+    {
+        GetCellByCoords = getCellByCoords;
+    }
+}
--- Assets/Scripts/CellFabric.cs
+++ Assets/Scripts/CellFabric.cs
@@ -8,13 +8,4 @@ using UnityEngine.UI;
 
 public static class CellFabric
 {
-    public static void RegisterCell(GameObject cellGameObject, (int, int) coords)//!coords should be written in (y, x) format
-    {
-        var cell = cellGameObject.AddComponent<Cell>();
-        cellGameObject.GetComponent<Button>().onClick.AddListener(() =>
-        {
-            GameController.OnTrySelectCoords.Invoke(coords);
-        });
-        GameController.CellsGameObjects[coords.Item1, coords.Item2] = cellGameObject;
-    }
 }
